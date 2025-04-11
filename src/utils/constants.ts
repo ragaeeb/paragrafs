d51a0546e250f2ac635be19ac71b6d1cac584972
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2025-04-11 13:59:55 -0400

    Erase all files

--- src/utils/constants.ts
+++
@@ -1,5 +0,0 @@
-export const MAX_CHUNK_DURATION = 60 * 5; // wit.ai supports up to 5 mins
-
-export const MIN_CHUNK_DURATION = 4; // minimum 4s duration
-
-export const MIN_CONCURRENCY = 1;
--- src/utils/logger.ts
+++
@@ -1,9 +0,0 @@
-import pino, { type Logger } from 'pino';
-import process from 'process';
-
-const logger: Logger = pino({
-    base: { hostname: undefined, pid: undefined }, // This will remove pid and hostname but keep time
-    level: process.env.LOG_LEVEL || 'info',
-});
-
-export default logger;
